99af71aadbca0648b8fa6b400da2356c2ca4c394
EternYang <y312193268@gmail.com>
2018-07-21 22:28:00 +0800

    add models of trade

--- apps/trade/models.py
+++ apps/trade/models.py
@@ -19,11 +19,12 @@ class ShoppingCart(models.Model):
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
-        verbose_name = "商品类别"
+        verbose_name = "购物车"
         verbose_name_plural = verbose_name
+        unique_together = ("user", "goods")
 
     def __str__(self):
-        return self.name
+        return "%s(%d)".format(self.goods.name, self.goods_num)
 
 
 class OrderInfo(models.Model):
@@ -31,16 +32,18 @@ class OrderInfo(models.Model):
     订单
     '''
     ORDER_STATUS = (
-        ("success", "成功"),
-        ("cancel", "取消"),
-        ("", "待支付")
+        ("TRADE_SUCCESS", "成功"),
+        ("TRADE_CLOSED", "超时关闭"),
+        ("WAIT_BUYER_PAY", "交易创建"),
+        ("TRADE_FINISHED", "交易结束"),
+        ("paying", "待支付"),
     )
 
-    user = models.ForeignKey(User, verbose_name=u'用户')
-    order_sn = models.CharField(max_length=30, unique=True, verbose_name="订单号")
-    trade_no = models.CharField(max_length=50, null=True, blank=True, verbose_name=u"")
-    pay_status = models.CharField(choices=ORDER_STATUS, max_length=10, verbose_name="订单支付状态")
-    post_script = models.CharField(max_length=11, verbose_name="订单留言")
+    user = models.ForeignKey(User, verbose_name='用户')
+    order_sn = models.CharField(max_length=30, null=True, blank=True, unique=True, verbose_name="订单号")
+    trade_no = models.CharField(max_length=100, unique=True, null=True, blank=True, verbose_name=u"交易号")
+    pay_status = models.CharField(choices=ORDER_STATUS, default="paying", max_length=30, verbose_name="订单状态")
+    post_script = models.CharField(max_length=200, verbose_name="订单留言")
     order_mount = models.FloatField(default=0.0, verbose_name="订单金额")
     pay_time = models.DateTimeField(null=True, blank=True, verbose_name="支付时间")
 
@@ -63,7 +66,7 @@ class OrderGoods(models.Model):
     '''
     订单商品详情
     '''
-    order = models.ForeignKey(OrderInfo, verbose_name="订单信息")
+    order = models.ForeignKey(OrderInfo, verbose_name="订单信息", related_name="goods")
     goods = models.ForeignKey(Goods, verbose_name="商品")
     goods_num = models.IntegerField(default=0, verbose_name="商品数量")
 
